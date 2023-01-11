1cd50a2828ea3edd496ca8aa62eb70476e38f6af
FDeaquino <fidel.deaquino@gmail.com>
2023-01-11 13:07:57 -0600

    resolvers and typeDefs setup, questions for tutor/TA

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,63 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
-        }
+        //get the user without their password and populate their saved books
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .popolate('savedBooks');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
+    },
+
+    Mutation: {
+        // create a user and a corresponding token
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        }, 
+        //login the user with their credentials, otherwise throw authentication errors
+        login: async (parent, { email, password }) => {
+
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        }, 
+        // add a book to the user's saved books list and prevent duplicate books from being saved by adding to set
+        // similar to addFriend in deep-thoughts project
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    //prevent duplicates, add book to savedBooks, will populate in me query
+                    { $addToSet: { savedBooks: {bookId: args.bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            // if not logged in/unathenticated user, throw error
+            throw new AuthenticationError('Please log in to save this book.')
+        },
+        //TODO: write the removeBook mutation
     }
 };
 
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,10 +2,53 @@
 const { gql } = require('apollo-server-express');
 
 // create our typeDefs
+// TODO: in saveBookInput does bookId need to be String or Int "bookId is not the _id, but the book's id value returned from Google's Book API" - Challenge notes
+//TODO: What is the type for image??? Would it be string since it'll return an image url/link?
+// TODO: how do you write removeBook in type Mutation? (removeBook(bookId: ___): User)
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type: Book {
+    bookId: _____
+    authors: [String]
+    description: String
+    title: String
+    image: _____
+    link: String
+  }
+
+  input SaveBookInput {
+    bookId: _____
+    authors: [String]
+    description: String
+    title: String
+    image: _____
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
-    helloWorld: String
+    me: User
+  }
+  
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SaveBookInput): User
+    removeBook: _____
   }
 `;
+
+
 // export the typeDefs
 module.exports = typeDefs;
